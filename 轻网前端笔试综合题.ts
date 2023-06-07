0bc7c60bff49ea4c984b2b73f158e1b47fbf5693
liyongshuai <liys0820@163.com>
2023-06-07 10:53:30 +0800

    feat: add tips

--- 轻网前端笔试综合题.ts
+++ 轻网前端笔试综合题.ts
@@ -2,7 +2,7 @@
  * 使用ts 写一个简易版的学生成绩录入系统
  * 要求：
  * 1、使用 es6 class 编写构造函数；
- * 2、可以通过调用方法向系统内存入学生数据（例如语数英）；
+ * 2、可以通过调用方法向系统内存入学生数据（例如语数英，注意可扩展性）；
  * 3、可以通过调用方法获取每门科目的平均值；
  * 4、可以根据学生的总分或者单科分数进行排序；（选做）
  * 5、可以查询某个学生的成绩，以及总分；（选做）
@@ -13,6 +13,10 @@
  * 1、请尽可能多的使用 ts 的特性，例如：泛型、接口、枚举等
  * 2、请尽可能多的使用 es6 的特性，例如：箭头函数、解构赋值、模板字符串等（如有必要可使用，非强制要求）
  * 3、尽可能地考虑代码的可读性、可扩展性、可维护性
+ * 重要事情说三遍：
+ * 注意代码的可读性、可扩展性、可维护性
+ * 注意代码的可读性、可扩展性、可维护性
+ * 注意代码的可读性、可扩展性、可维护性
  */
 
 // 学生类
@@ -43,8 +47,8 @@ class ScoreSystem {
 
   /**
    * 必做
-   * @description: 获取每门科目或者总分的平均值
-   * @return {number}
+   * @description: 获取每门科目和总分的平均值
+   * @return {Object} {科目|总分: 平均分}
    */  
   getAverageScore() {
     // 请在此处完善代码
@@ -55,24 +59,24 @@ class ScoreSystem {
    * @description: 根据学生的总分或者单科分数进行排序
    * @return {Array}  {Student[]}
    */
-  sortScore() {
+  sortScore(subject = 'totalScore') {
     // 请在此处完善代码
   }
 
 
   /**
    * 选做
-   * @description: 查询某个学生的成绩，以及总分
+   * @description: 查询某个学生的各科成绩，以及总分
    * @param {string} name
-   * @return {Object | null}  {Student} | null
+   * @return {Object | null}  {科目|总分: 分数}
    */
-  findStudent(name: string) {
+  findStudentScore(name: string) {
     // 请在此处完善代码
   }
 
   /**
    * 选做
-   * @description: 删除某个学生的成绩
+   * @description: 删除某个学生
    * @param {string} name
    * @return {Boolean | Error}
    * */
